1de83c6165a9e344d8d58ac9988351aa09981195
hakimbalestrieri <hakim.balestrieri@heig-vd.ch>
2020-04-21 20:32:53 +0200

    ajout tests ControllerInscriptionTest

--- src/test/java/ch/heigvd/hbcg/ControllerInscriptionTest.java
+++ src/test/java/ch/heigvd/hbcg/ControllerInscriptionTest.java
@@ -1,13 +1,41 @@
 package ch.heigvd.hbcg;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.io.RandomAccessFile;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ControllerInscriptionTest {
 
+    @BeforeEach
+    void setUp() {
+    }
+
     @Test
     void createInscription() {
+        try {
+            //premiere inscription, l'utilisateur n'existe pas encore
+            assertEquals(1,ControllerInscription.createInscription("testUser", "chemindutest", "password", true, 24,10,1993,"Homme"));
+            //seconde inscription, l'utilisateur existe déjà et devrait renvoyer 0
+            assertEquals(0, ControllerInscription.createInscription("testUser", "chemindutest", "password", true, 24,10,1993,"Homme"));
+            //apres ce test il faut supprimer la dernière ligne du fichier pour pouvoir lancer ce test à nouveau dans de bonnes conditions.
+
+            try{
+                RandomAccessFile raf = new RandomAccessFile("users.txt", "rw");
+                long length = raf.length();
+                raf.setLength(length - 8);
+                raf.close();
+            }catch(Exception ex){
+                ex.printStackTrace();
+            }
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Test
